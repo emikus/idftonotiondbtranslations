fad528b2f0b5162a5ab233121c02a256f00138c8
em_iot@wp.pl <em_iot@wp.pl>
2022-06-06 12:51:05 +0200

    getting the word for translation from NotionForms web hook request

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import * as notionDBModyfier from "./notionDBModyfier.js";
 import util from 'util';
 
 // process.env.PORT ||
-const PORT =  process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 
 var wordTranslations = {
   Swedish: "Swedish",
@@ -41,53 +41,22 @@ var wordTranslations = {
 
 
 
-    http.createServer(function (req, res) {
-
-
-      console.log(444444444)
-
-      let data = '';
+http.createServer(function (req, res) {
+  let data = '';
   req.on('data', chunk => {
     data += chunk;
     console.log(111111111111111)
   });
   req.on('end', () => {
     console.log(2222222222222)
-    console.log(JSON.parse(data)); // 'Buy the milk'
-
-
-    const wordToTranslate = queryObject.wordToTranslate
-              // console.log(wordToTranslate)
-              // TranslationsUtils.getTranslationsFromIDF(wordToTranslate, Object.keys(wordTranslations))
-              // .then(function(wordTranslations) {
-              //   notionDBModyfier.addTranslationsToNotionDB(wordTranslations)
+    console.log(); // 'Buy the milk'
+    const wordToTranslate = JSON.parse(data).submission.WordInEnglish
+    TranslationsUtils.getTranslationsFromIDF(wordToTranslate, Object.keys(wordTranslations))
+      .then(function (wordTranslations) {
+        notionDBModyfier.addTranslationsToNotionDB(wordTranslations)
+      })
     res.end();
   });
-        switch(req.url){
-            case '/':         res.write('website.com');          break;
-            case '/about_us': res.write('website.com/about_us'); break;
-            case '/api':  
-// console.log("-------------++++++++++++++++___________+++++++++++++++++++---------------")
-// req.on('data', function (chunk) {
-//   console.log('BODY: ' + chunk);
-// });
-// // console.log(util.inspect(res, false, null, true /* enable colors */))
-// console.log("-------------++++++++++++++++___________+++++++++++++++++++---------------")
-
-
-
-              // const queryObject = url.parse(req.url, true).
-              // // const queryObject = url.parse(req.url, true).query;
-              // const wordToTranslate = queryObject.wordToTranslate
-              // console.log(wordToTranslate)
-              // TranslationsUtils.getTranslationsFromIDF(wordToTranslate, Object.keys(wordTranslations))
-              // .then(function(wordTranslations) {
-              //   notionDBModyfier.addTranslationsToNotionDB(wordTranslations)
-              // })
-            //   break;
-            // default:          res.write('website.com/error_404');
-        }
-        res.end();
 }).listen(PORT);
 
 
